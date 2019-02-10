9ae94cebc4da7c2c25b67a4690011695cc7a74d2
yulia_norkina <yulia.norkina2@gmail.com>
2019-02-10 17:01:38 +0200

    create fragment in activity

--- app/src/main/java/com/example/factasynctask/MainActivity.java
+++ app/src/main/java/com/example/factasynctask/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.factasynctask;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,8 +9,11 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final String TAG_TASK_FRAGMENT = "task_fragment";
     private EditText etNumber;
     private TextView tvResult;
+    private FragmentManager fm;
+    private FragmentAsyncTask mTaskFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         etNumber = findViewById(R.id.etNumber);
         tvResult = findViewById(R.id.tvResult);
         findViewById(R.id.bCalculate).setOnClickListener(this);
+
+        fm = getSupportFragmentManager();
+        mTaskFragment = (FragmentAsyncTask) fm.findFragmentByTag(TAG_TASK_FRAGMENT);
+        if (mTaskFragment == null) {
+            Bundle bundle = new Bundle();
+            mTaskFragment = new FragmentAsyncTask();
+            mTaskFragment.setArguments(bundle);
+            fm.beginTransaction().add(mTaskFragment, TAG_TASK_FRAGMENT).commit();
+        }
     }
 
     @Override
