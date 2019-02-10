0f03cfbfae00eb7814dd28a1dbc38cd3bccaa0d3
yulia_norkina <yulia.norkina2@gmail.com>
2019-02-10 16:33:54 +0200

    add Async Task to Fragment

--- app/src/main/java/com/example/factasynctask/FragmentAsyncTask.java
+++ app/src/main/java/com/example/factasynctask/FragmentAsyncTask.java
@@ -1,9 +1,12 @@
 package com.example.factasynctask;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+
+import java.lang.ref.WeakReference;
 
 public class FragmentAsyncTask extends Fragment {
     private TaskCallbacks callback;
@@ -26,5 +29,36 @@ public class FragmentAsyncTask extends Fragment {
     @Override
     public void onDetach() {
         super.onDetach();
+    }
+
+    private static class MyAsyncTask extends AsyncTask<Integer, Void, Integer> {
+
+        private WeakReference<FragmentAsyncTask> taskFragment;
+
+        MyAsyncTask(FragmentAsyncTask context) {
+            taskFragment = new WeakReference<>(context);
+        }
+
+        @Override
+        protected Integer doInBackground(Integer... integers) {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            int fact = 1;
+            for (int count = integers[0]; count > 1; count--) {
+                fact = fact * count;
+            }
+            return fact;
+        }
+
+        @Override
+        protected void onPostExecute(Integer integer) {
+            super.onPostExecute(integer);
+            FragmentAsyncTask fragment = taskFragment.get();
+            if (fragment == null) return;
+            fragment.callback.onPostExecute(integer);
+        }
     }
 }
